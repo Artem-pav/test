d4501dde3e95c89f9dd94c730fa0e005f3588b46
Artem <art.pavlushenko@gmail.com>
2022-06-30 21:01:48 +0300

    filform2

--- js/012 script.js
+++ js/012 script.js
@@ -23,7 +23,8 @@
 
 'use strict';
 
-const numberOfFilms = prompt ("Сколько фильмов вы уже посмотрели?", "количесво");
+const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", " ");
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -31,6 +32,11 @@ const personalMovieDB = {
     genres:[],
     privat: false
 };
-const nameFilm = prompt ("Один из последних просмотренных фильмов", "имя");
-const raiting = prompt ('На сколько оцените его?', '10');
-console.log(personalMovieDB.count,nameFilm,raiting);
+ const a = prompt ("Один из последних просмотренных фильмов", " "),
+       b = prompt ('На сколько оцените его?', ''),
+       c = prompt ("Один из последних просмотренных фильмов", " "),
+       d= prompt ('На сколько оцените его?', ' ');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
+
