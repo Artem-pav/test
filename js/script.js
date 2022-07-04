4506922a91105b2b965a16d395db809f2efa4236
Artem <art.pavlushenko@gmail.com>
2022-07-04 21:40:53 +0300

    Home2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 "use strict";
-const user = 'Artem';
 
+  
+ /*while (num<= 55) {
+        console.log(num);
+        num++;
+ }*/
+ let num = 50;
+for(let i = 1;i < 8; i++) {
+    console.log(num);
+    num++;
+}
