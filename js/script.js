d125c63c3340c6fa3abaac2cf5089753c2e775e8
Artem <art.pavlushenko@gmail.com>
2022-07-07 19:55:10 +0300

    fuctions

--- js/script.js
+++ js/script.js
@@ -10,3 +10,23 @@ for(let i = 1;i < 8; i++) {
     console.log(num);
     num++;
 }
+
+
+function showFirstMessage(text) {
+   console.log(text)
+}
+showFirstMessage('Hi');
+
+
+function calc(a,b) {
+    return (a + b);
+}
+
+console.log(calc(5,6));
+
+const logger = function() {
+      console.log("hi")
+};
+logger();
+
+const calcS = (a,b) => a + b;
